8eaf8b10f84558088a3b41fd0595dd9d19c9a06d
Alex Bandisch <alexander.bandisch@gmail.com>
2018-04-17 15:05:07 +0100

    Try to automate now deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gym-tracker-server';
+exports.DATABASE_URL = process.env.NODE_ENV === 'development' ? 'mongodb://localhost/gym-tracker-server' : process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/gym-tracker-server-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'mytestingsecret';
